84418a7597e2cd9687a133098704c07afc09185f
victorvos <me@victorvos.com>
2018-05-30 17:11:38 +0200

    Changes

---
+++ src/test/java/Testing.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+
+public class Testing {
+
+    @Test
+    public void test_fail() {
+        fail("Let's test the failing unit test");
+    }
+}
