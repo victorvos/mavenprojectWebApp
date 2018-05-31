7eb0aadf1f150c8b7b838513f33fd254880d06ac
victorvos <me@victorvos.com>
2018-05-31 13:55:03 +0200

    added mvn sonar push command in comment

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -11,3 +11,5 @@ public class Testing {
         fail("Let's test the failing unit test");
     }
 }
+
+ //   mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dmaven.test.failure.ignore=true -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=friendspammer -Dsonar.login=3b2efdf08c061da5104a594afdf6565db69c2a05
